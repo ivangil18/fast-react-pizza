75e325ee4c6b118c814013f0ce17c075db4db90b
Ivan Gil <igil.dev@gmail.com>
2023-08-26 10:28:12 -0400

    loader implemented with useNavigation hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
@@ -11,7 +12,7 @@ const router = createBrowserRouter([
     element: <AppLayout />, //this is a layout route - doesn't require a path
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      { path: "/menu", element: <Menu />, loader: menuLoader }, //loader accepts a function in charge of fetching data from an external source (API)
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:orderId", element: <Order /> },
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,18 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
+  //useNavigation hook provide the state of the navigation (idle, loading, submitting),
+  //this information is available in the entire application regardless of the componenet triggering it
+  const navigation = useNavigation();
+
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
